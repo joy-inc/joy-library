c8fd72fb3f6d61d7e36d67ce4427edb5e0e27ec1
shiwen.dai <shiwen.dai@qyer.com>
2015-07-08 14:55:08 +0800

    add MainActivity

---
+++ app/src/main/java/com/easylinknj/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.easylinknj.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.easylinknj.R;
+
+/**
+ * Created by KEVIN.DAI on 15/7/8.
+ */
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
