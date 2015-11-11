afabbb9b17a99af2416365d60696de5f5f106dfd
shiwen.dai <shiwen.dai@qyer.com>
2015-11-11 11:22:27 +0800

    删除获取versionCode和versionName的代码，用BuildConfig代替

--- library/src/com/joy/library/utils/AppUtil.java
+++ library/src/com/joy/library/utils/AppUtil.java
@@ -2,7 +2,6 @@ package com.joy.library.utils;
 
 import android.content.Context;
 import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 
 import com.joy.library.BaseApplication;
 
@@ -32,42 +31,5 @@ public class AppUtil {
         }
 
         return firstInstallTime;
-    }
-    /**
-     * 获取VersionCode，默认返回1
-     * @return
-     */
-    public static int getVersionCode() {
-
-        try {
-
-            Context ctx = BaseApplication.getContext();
-            PackageInfo packInfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0);
-            return packInfo.versionCode;
-
-        } catch (PackageManager.NameNotFoundException e) {
-
-//            if(LogMgr.isDebug())
-                e.printStackTrace();
-        }
-
-        return 1;
-    }
-
-    public static String getVersionName() {
-
-        try {
-
-            Context ctx = BaseApplication.getContext();
-            PackageInfo packInfo = ctx.getPackageManager().getPackageInfo(ctx.getPackageName(), 0);
-            return packInfo.versionName == null ? "" : packInfo.versionName;
-
-        } catch (Exception e) {
-
-//            if(LogMgr.isDebug())
-                e.printStackTrace();
-        }
-
-        return "";
     }
 }
