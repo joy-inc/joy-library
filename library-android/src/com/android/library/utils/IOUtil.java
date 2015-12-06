f114cc127d6ca8434110e86342a1bba743ecfdbf
liulongzhenhai <longzhenhai.liu@qyer.com>
2015-12-06 21:22:30 +0800

    修改分享

--- library-android/src/com/android/library/utils/IOUtil.java
+++ library-android/src/com/android/library/utils/IOUtil.java
@@ -1,5 +1,7 @@
 package com.android.library.utils;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
 
@@ -30,5 +32,35 @@ public class IOUtil {
 
             e.printStackTrace();
         }
+    }
+
+    public static boolean copyFileFromRaw(InputStream in, File outPutFile) {
+
+        boolean result = false;
+        FileOutputStream fos = null;
+
+        try {
+
+            fos = new FileOutputStream(outPutFile);
+
+            byte[] buffer = new byte[1024];
+            int count = 0;
+            while ((count = in.read(buffer)) != -1) {
+
+                fos.write(buffer, 0, count);
+            }
+
+            result = true;
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        } finally {
+
+            closeInStream(in);
+            closeOutStream(fos);
+        }
+
+        return result;
     }
 }
---
+++ library-joy/src/main/java/com/joy/library/share/ShareType.java
@@ -0,0 +1,16 @@
+package com.joy.library.share;
+
+/**
+ * 分享的类型
+ * User: liulongzhenhai(longzhenhai.liu@qyer.com)
+ * Date: 2015-12-06
+ */
+public enum  ShareType {
+
+    SINA,
+    WEIXIN,
+    WEIXIN_CIRCLE,
+    EMAIL,
+    SMS,
+    MORE;
+}
